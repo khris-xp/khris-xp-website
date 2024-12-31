535b7ede43d31af7f12c3121169d321c84b34683
khris-xp <khrisbharmmano@gmail.com>
2024-12-31 16:19:38 +0700

    fix: calculate year function

--- src/script/date.ts
+++ src/script/date.ts
@@ -1,21 +1,21 @@
 export function calculateExperingDate(startDate: string, endDate: string) {
   const start = new Date(startDate);
-  let end;
-  if (endDate === 'Present') {
-    end = new Date();
-  } else {
-    end = new Date(endDate);
-  }
+  const end = endDate === "Present" ? new Date() : new Date(endDate);
 
   const diff = end.getTime() - start.getTime();
-  const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
-  const months = Math.floor(
+  let years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
+  let months = Math.floor(
     (diff % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24 * 30)
   );
 
+  if (months >= 12) {
+    years += Math.floor(months / 12);
+    months = months % 12;
+  }
+
   return years === 0
     ? months === 0
-      ? 'Less than a month'
+      ? "Less than a month"
       : `${months} months`
     : months === 0
     ? `${years} years`
